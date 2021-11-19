6058cf5c4203b2a16553346e04a3ad48422ac180
AhammedGhani <ahamedghanise@gmail.com>
2021-11-19 18:26:23 +0530

    Cors added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express") ;
 const app = express();
 require('dotenv').config()
+const cors = require('cors')
 const isAuthenticated = require("./middlewares/authorization-middleware.js");
 
+
+
+
 const PORT = process.env.PORT || 3001 ;
+app.use(cors());
 
 // app routers
 const signupRouter = require("./routes/signup-route");
--- src/routes/classroom-route.js
+++ src/routes/classroom-route.js
@@ -95,4 +95,46 @@ router.patch("/",isAuthenticated,express.urlencoded(),(req,res)=>{
     })
 })
 
+router.post("/join",isAuthenticated,isStudent,(req,res)=>{
+    // check if req already exists
+    //  if yes res = failure,message:"already exists"
+    // else add to joinRequests {classroomId,req.uid->student id}
+    // check error
+
+
+})
+
+//accept join request API, POST classroom/student/accept
+//accept join request API, POST classroom/student/reject
+// isTeacher both
+// inp->{student:id}
+// delete from joinRequests, if accept add in enrolledStudent
+
+// remove student request API, POST classroom/student/remove
+// isTeacher
+// inp->{student : id} remove student from enrolled student
+
+
+
+router.get("/:id",isAuthenticated,express.urlencoded(), (req,res)=>{
+    const classroomId = req.params.id;
+    firestore.collection('classroom').doc(classroomId).get()
+    .then((docRef)=>{
+        res.status(200).json({
+            status : "success",
+            message : docRef.data()
+        })
+
+    }).catch((error)=>{
+        res.status(400).json({
+            status : "FAILURE",
+            message : error.message
+        })
+        
+    });
+
+
+})
+
+
 module.exports = router ;
